0a342e5b2e6103ae7145f5af478347167f810f37
devrathrana <devrath.rana@bold.com>
2024-11-21 13:58:40 +0530

    add other types of console

--- 1_console/script.js
+++ 1_console/script.js
@@ -1,13 +1,13 @@
 const logSomething = (logType) => {
   switch (logType) {
     case "INFO":
-      console.log("Hello");
+      console.log("Hello normal");
       break;
     case "WARN":
-      console.warn("Hello");
+      console.warn("Hello warning");
       break;
     case "ERROR":
-      console.error("Hello");
+      console.error("Hello error");
       break;
     case "TABLE":
       console.table([
@@ -20,6 +20,29 @@ const logSomething = (logType) => {
           last: "Sita",
         },
       ]);
+      break;
+    case "GROUP":
+      const label = "Adolescent Hey";
+      console.group(label);
+      console.info("Leo");
+      console.info("Mike");
+      console.groupEnd(label);
+      break;
+    case "CUSTOM":
+      const spacing = "5px";
+      const styles = `padding:${spacing};background-color:darkblue;color:white;font-style:italic;border:${spacing} solid crimson;font-size:2em;`;
+      console.log("%cEternal soul.", styles);
+      break;
+    case "ERROR-404":
+      fetch("https://abckuchbhi.com");
+      break;
+    case "VIOLATION":
+      console.debug("Hello verbose error!");
+      const duration = 3000;
+      const start = new Date().getTime();
+      while (new Date().getTime() < start + duration) {
+        // block the main thread for 3 seconds
+      }
       break;
     default:
       console.warn("Select valid log option!");
