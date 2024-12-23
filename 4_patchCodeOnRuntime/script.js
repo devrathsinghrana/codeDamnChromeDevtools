01a2f8e9d0d7079691702ad18bcf0eb8cda056dc
devrathrana <devrath.rana@bold.com>
2024-12-23 22:13:50 +0530

    js

---
+++ 4_patchCodeOnRuntime/script.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+  consoleLogSomething();
+}, 1000);
+
+function consoleLogSomething() {
+  console.log("something not funny!");//we can modify this text on source code and see the result
+}
